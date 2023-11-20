4ae57687ec03d8e75c91d8002f43f475573b81ef
pooja <poojak1447@gmail.com>
2023-11-20 17:29:05 +0530

    add before the given node

---
+++ addbeforegivennode_singlylinkedlist.py
@@ -0,0 +1,67 @@
+class Node:
+    def __init__(self,data) -> None:
+        self.data=data
+        self.ref=None
+class linked_list:
+    def __init__(self) -> None:
+        self.head=None
+    def print_LL(self):
+        if self.head is None:
+            print("linked list is empty")
+        else:
+            n=self.head
+            while n is not None:
+                print(n.data)
+                n=n.ref
+    def add_begin(self,data):
+        new_node=Node(data)
+        new_node.ref=self.head
+        self.head=new_node
+    def add_end(self,data):
+        new_node=Node(data)
+        if self.head is None:
+            self.head=new_node
+        else:
+            n=self.head
+            while n.ref is not None:
+                n=n.ref
+            n.ref=new_node
+    def after_node(self,data,x):
+        n=self.head
+        while n is not None:
+            if x==n.data:
+                break
+            else:
+                n=n.ref
+        if n is None:
+            print("node is not present in the list")
+        else:
+            new_node=Node(data)
+            new_node.ref=n.ref
+            n.ref=new_node
+    def add_before(self,data,x):
+        if self.head is None:
+            print("linked list is empty")
+        if self.head.data==x:
+            new_node=Node(data)
+            new_node.ref=self.head
+            self.head=new_node
+        n=self.head
+        while n.ref is not None:
+            if n.ref.data==x:
+                break
+            n=n.ref
+        if n.ref is None:
+            print("node is not found")
+        else:
+            new_node=Node(data)
+            new_node.ref=n.ref
+            n.ref=new_node
+LL1=linked_list()
+LL1.add_begin(10)
+LL1.add_before(20,10)
+LL1.add_before(30,10)
+LL1.print_LL()
+        
+
+        
