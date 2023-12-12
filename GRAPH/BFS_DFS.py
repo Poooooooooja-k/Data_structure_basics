f232cde3cf95fd28161969aee05683c58aa61948
pooja <poojak1447@gmail.com>
2023-12-12 15:33:57 +0530

    Bfs

--- GRAPH/BFS_DFS.py
+++ GRAPH/BFS_DFS.py
@@ -1,4 +1,4 @@
-
+from collections import deque
 def add_node(v):
     global node_count
     if v in nodes:
@@ -32,19 +32,41 @@ def DFS(start_node, visited):
     for i in range(node_count):
         if graph[index][i] == 1 and not visited[i]:
             DFS(nodes[i], visited)
+def BFS(start_node,visited):
+    if start_node not in nodes:
+        print("node not present")
+    queue=deque()
+    queue.append(start_node)
+    while queue:
+        current=queue.popleft()
+        print(current,end=" ")
+        visited[nodes.index(current)]=True
+        for i in range(node_count):
+            if graph[nodes.index(current)][i]==1 and not visited[i]:
+                queue.append(nodes[i])
+                visited[i]=1
 nodes=[]
 graph=[]
 node_count=0
 print("before addimg nodes")
 print(nodes)
 add_node("A")
+add_node("A")
 add_node("B")
 add_node("C")
-add_node("Y")
-add_edge("A","B")
+add_node("D")
+add_node("E")
+add_node("M")	
+add_edge("A", "B")
 add_edge("A", "C")
+add_edge("B", "C")
+add_edge("C", "D")
+add_edge("D", "f")
+print()
 print(nodes)
 print(graph)
 visited = [False]*node_count
 DFS("A", visited)
+print()
+BFS("A", visited)
 print()
